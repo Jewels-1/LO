60340438f38fc1f044fb842f9a664503401dab79
Jewels-1 <92256678+Jewels-1@users.noreply.github.com>
2023-01-25 17:17:46 +0000

    Add files via upload

---
+++ pedrotti3p18.m
@@ -0,0 +1,14 @@
+P = (1000)*pi*(2.5/100)^2 %Watts = (W/m^2)*(m^2)
+
+%For an image at infinity, the image is produce at the focal length of the
+%thin lens and in the paraxial approximation has an image height
+%(theta/2)*f
+
+A = pi*((0.25*pi/180)*50/100)^2 %m^2
+
+Irradiance = (P/A) *(1/100)^2 %W/cm^2 = (W/m^2)*(1 m /100 cm)^2
+
+%It looks like the units are incorrect in the back of the book or that a
+%calculation mistake was made by the author
+%verified that the irradiance of the sun at earth surface can be
+%approximately 1000 W/m^2
